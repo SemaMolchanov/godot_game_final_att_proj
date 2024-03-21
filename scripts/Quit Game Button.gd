26c43dc6167ae52f97e1cd6627bed55d3cf983e4
Sema Molchanov <sema.molchanov@gmail.com>
2024-03-21 18:03:12 +0600

    project structure built

--- scripts/Quit Game Button.gd
+++ scripts/Quit Game Button.gd
@@ -1,8 +1,7 @@
 extends Button
 
 
-
 func _on_pressed():
 	if get_tree().paused == true:
 		get_tree().paused = false
-	get_tree().change_scene_to_file("res://scenes/main_menu.tscn")
+	get_tree().change_scene_to_file("res://scenes/menus/main_menu.tscn")
